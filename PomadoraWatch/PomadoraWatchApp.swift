208df22704599d0d366eef3622f3f599cc9e4322
Bowen1248 <bowenlee31@gmail.com>
2023-05-01 08:54:06 +0800

    Initial Commit

---
+++ PomadoraWatch/PomadoraWatchApp.swift
@@ -0,0 +1,17 @@
+//
+//  PomadoraWatchApp.swift
+//  PomadoraWatch
+//
+//  Created by hsnl on 2023/5/1.
+//
+
+import SwiftUI
+
+@main
+struct PomadoraWatchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
